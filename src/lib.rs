ef1c9fdad7b500a0f43074df5c705de437962d8c
John.Koepi <john.koepi@gmail.com>
2017-11-17 22:22:09 +0200

    quote text

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,13 @@
 //! This tree implementation uses encoding scheme as in _Certificate Transparency_
 //! RFC 6962 (https://tools.ietf.org/html/rfc6962):
 //!
-//! > MTH({d(0)}) = ALG(0x00 || d(0)).
-//! > For n > 1, let k be the largest power of two smaller than n (i.e.,
-//! > k < n <= 2k).  The Merkle Tree Hash of an n-element list D[n] is then
-//! > defined recursively as
-//! > MTH(D[n]) = ALG(0x01 || MTH(D[0:k]) || MTH(D[k:n])),
+//! ```text
+//! MTH({d(0)}) = ALG(0x00 || d(0)).
+//! For n > 1, let k be the largest power of two smaller than n (i.e.,
+//! k < n <= 2k).  The Merkle Tree Hash of an n-element list D[n] is then
+//! defined recursively as
+//! MTH(D[n]) = ALG(0x01 || MTH(D[0:k]) || MTH(D[k:n])),
+//! ```
 //!
 //! Link: https://en.wikipedia.org/wiki/Merkle_tree
 //!
